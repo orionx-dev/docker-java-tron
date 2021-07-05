bef85073e2a641c19d8575aeeaab3fde6a45b420
paltaa <jquiroga1991@gmail.com>
2021-07-05 17:16:01 -0400

    feat: improve readme and build script for future reference
    Signed-off-by: paltaa <jquiroga1991@gmail.com>

--- build.sh
+++ build.sh
@@ -1,1 +1,7 @@
-docker build . --build-arg JAVA_TRON_VERSION=GreatVoyage-v4.2.0 -t 658464581062.dkr.ecr.us-east-1.amazonaws.com/orionx/java-tron:4.2.0
+VERSION=(${1//-v/ })
+
+REGISTRY=658464581062.dkr.ecr.us-east-1.amazonaws.com/orionx/java-tron
+
+docker build . --build-arg JAVA_TRON_VERSION=${1} -t ${REGISTRY}:${VERSION[1]}
+
+docker push ${REGISTRY}:${VERSION[1]}
